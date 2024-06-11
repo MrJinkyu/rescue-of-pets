1d59e6a86b8fe0d3c7e21dbd0effc40482ae09f6
JinKyu <rlawlsrb167@naver.com>
2024-06-11 22:55:16 +0900

    feat: 스토리페이지 스켈레톤 UI

--- app/(pages)/story/loading.tsx
+++ app/(pages)/story/loading.tsx
@@ -5,19 +5,27 @@ export default function StoryLoading() {
         return (
           <div
             key={index}
-            className="w-full h-40 px-5 flex items-center animate-pulse"
+            className="w-full h-72 flex flex-col justify-center animate-pulse"
           >
-            <div className="relative size-32 bg-neutral-400 rounded-md"></div>
-            <div className="flex flex-col px-2 gap-1">
+            <div className="w-full px-4 py-3 flex justify-between items-center">
               <div className="flex items-center gap-2">
-                <div className="p-1 w-8 h-5 rounded-md bg-neutral-400" />
-                <div className="p-1 w-6 h-5 rounded-md bg-neutral-400" />
+                <div className="bg-neutral-400 rounded-full flex justify-center items-end size-8" />
+                <span className="bg-neutral-400 rounded-md w-8 h-4" />
               </div>
-              <div className="w-24 h-5 bg-neutral-400 rounded-md" />
-              <div className="w-24 h-5 bg-neutral-400 rounded-md" />
-              <div className="w-24 h-5 bg-neutral-400 rounded-md" />
-              <div className="w-24 h-5 bg-neutral-400 rounded-md" />
+              <div className="bg-neutral-400 rounded-md w-8 h-4" />
             </div>
+            <main className="px-4 pb-3">
+              <h3 className="mb-3 w-1/3 h-5 bg-neutral-400 rounded-sm"></h3>
+              <div className="w-2/3 h-4 bg-neutral-400 rounded-sm" />
+              <div className="flex items-center">
+                <div className="size-28 rounded-sm bg-neutral-400 my-3" />
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="w-10 h-5 rounded-sm bg-neutral-400" />
+                <span className="w-10 h-5 rounded-sm bg-neutral-400" />
+                <span className="w-10 h-5 rounded-sm bg-neutral-400" />
+              </div>
+            </main>
           </div>
         );
       })}
