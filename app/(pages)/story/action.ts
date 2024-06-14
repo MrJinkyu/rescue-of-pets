314577ff1ffad05063fdef275b2003e1f6d7485e
JinKyu <rlawlsrb167@naver.com>
2024-06-14 16:13:42 +0900

    feat: 스토리 페이지 무한스크롤

--- app/(pages)/story/action.ts
+++ app/(pages)/story/action.ts
@@ -26,6 +26,37 @@ export async function getInitStory() {
     orderBy: {
       createdAt: "desc",
     },
+    take: 5,
+  });
+  return storys;
+}
+
+export async function getMoreStory(page: number) {
+  const storys = await prismaDB.story.findMany({
+    select: {
+      id: true,
+      title: true,
+      contents: true,
+      createdAt: true,
+      photo: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+      user: {
+        select: {
+          avatar: true,
+          username: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 5,
+    skip: page * 5,
   });
   return storys;
 }
--- components/(story)/story-list.tsx
+++ components/(story)/story-list.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 import StoryCard from "./story-card";
+import { getMoreStory } from "@/app/(pages)/story/action";
 
 interface StoryListProps {
   initList: {
@@ -20,11 +23,48 @@ interface StoryListProps {
 }
 
 export default function StoryList({ initList }: StoryListProps) {
+  const [posts, setPosts] = useState(initList);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const trigger = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        observer: IntersectionObserver
+      ) => {
+        const element = entries[0];
+        if (trigger.current && element.isIntersecting) {
+          observer.unobserve(trigger.current);
+          const nextPosts = await getMoreStory(page + 1);
+
+          if (nextPosts.length !== 0) {
+            setPosts((prev) => [...prev, ...nextPosts]);
+            setPage((prev) => prev + 1);
+          } else {
+            setIsLastPage(true);
+          }
+        }
+      },
+      {
+        rootMargin: "0px 0px -73px 0px",
+      }
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
+
   return (
     <div className="bg-neutral-100 flex flex-col gap-1">
-      {initList.map((post) => (
+      {posts.map((post) => (
         <StoryCard key={post.id} {...post} />
       ))}
+      {!isLastPage && <span ref={trigger} className="w-full h-0" />}
     </div>
   );
 }
