75f64b33fe3ee96fce30512eafdb5f7abc4b1474
JinKyu <rlawlsrb167@naver.com>
2024-06-26 17:15:35 +0900

    feat: 임시보호 페이지 성별 미확인 시 UI 가리기

--- components/(home)/temporary-protection-gender.tsx
+++ components/(home)/temporary-protection-gender.tsx
@@ -3,12 +3,15 @@ export default function TemporaryProtectionGender({
 }: {
   gender: string;
 }) {
+  if (gender === "미확인") {
+    return null;
+  }
   return (
     <div
       className={`${
         gender === "암컷"
-          ? "border-pink-100 text-pink-600"
-          : "border-blue-100 text-blue-600"
+          ? "border-pink-200 text-pink-600"
+          : "border-blue-200 text-blue-600"
       } p-1 rounded-md text-xs font-semibold border-2`}
     >
       {gender}
--- components/(home)/temporary-protection-isActive.tsx
+++ components/(home)/temporary-protection-isActive.tsx
@@ -7,8 +7,8 @@ export default function TemporaryProtectionIsActive({
     <div
       className={`${
         isActive
-          ? "border-green-100 text-green-600"
-          : "border-neutral-100 text-neutral-600"
+          ? "border-green-200 text-green-600"
+          : "border-neutral-200 text-neutral-600"
       } p-1 rounded-md text-xs font-semibold border-2 `}
     >
       {isActive ? "보호중" : "완료"}
