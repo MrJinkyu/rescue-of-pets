cb823d01c7c54cd312ef339dbb60c25848516dd3
JinKyu <rlawlsrb167@naver.com>
2024-06-28 17:25:23 +0900

    chore: 메타데이터 open graph 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,30 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: { template: "%s - meet again", default: "meet again" },
-  description: "반려동물 입양, 반려동물 실종신고",
+  title: { template: "%s | meet again", default: "meet again" },
+  description:
+    "반려동물 입양, 유기동물 실종신고 등 동물과 사람을 이어주는 서비스",
+  keywords: [
+    "meet again",
+    "반려동물",
+    "유기동물",
+    "동물입양",
+    "임시보호",
+    "실종신고",
+    "일상기록",
+  ],
+  openGraph: {
+    title: "meet again",
+    description:
+      "반려동물 입양, 유기동물 실종신고 등 동물과 사람을 이어주는 서비스",
+    siteName: "meet again",
+    locale: "ko_KR",
+    type: "website",
+    url: "https://rescue-of-pets.vercel.app",
+    images: {
+      url: "/again.png",
+    },
+  },
   icons: {
     icon: "/meet.png",
   },
