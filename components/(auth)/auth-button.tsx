eba2adfd9e2be856caadf4515fd0cb3bf8e36132
JinKyu <rlawlsrb167@naver.com>
2024-07-05 05:19:45 +0900

    style: auth-button에 loading spinner UI 추가

--- components/(auth)/auth-button.tsx
+++ components/(auth)/auth-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useFormStatus } from "react-dom";
+import LoadingSpinner from "../common/loading-spinner";
 
 interface AuthButtonProps {
   text: string;
@@ -8,14 +9,16 @@ interface AuthButtonProps {
 
 export default function AuthButton({ text }: AuthButtonProps) {
   const { pending } = useFormStatus();
+
   return (
     <button
       disabled={pending}
-      className={`border-none outline-none flex-1 mt-4 bg-blue-500 text-white font-semibold p-3 rounded-md ${
-        pending ? "opacity-80" : ""
+      className={`border-none outline-none flex-1 mt-4 flex items-center justify-center gap-1 bg-blue-500 text-white font-semibold p-3 rounded-md ${
+        pending ? "bg-mainColor/40 cursor-wait" : ""
       }`}
     >
-      {pending ? "로딩중" : text}
+      {pending && <LoadingSpinner size="sm" />}
+      <span>{pending ? "로딩중.." : text}</span>
     </button>
   );
 }
