ff10e5686ba70653da0deb73f19e57bf486af10b
JinKyu <rlawlsrb167@naver.com>
2024-07-05 05:28:33 +0900

    fix: 회원가입 비밀번호 정규표현식 특수문자 사용 가능

--- constants/password.ts
+++ constants/password.ts
@@ -2,5 +2,5 @@ export const PASSWORD_MIN_LENGRH = 8;
 export const PASSWORD_REGEX_ERROR_MESSAGE =
   "비밀번호는 숫자 영문을 포함하여 8자 이상입니다";
 export const PASSWORD_REGEX = new RegExp(
-  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\S]{8,}$/
 );
