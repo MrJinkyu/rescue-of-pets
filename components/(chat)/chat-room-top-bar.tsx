7da091f6c9111b59c2bfbbe7740d919f4dc3c288
JinKyu <rlawlsrb167@naver.com>
2024-07-05 04:50:21 +0900

    fix: 채팅 top bar 뒤로가기 시 페이지 reload

--- components/(chat)/chat-room-top-bar.tsx
+++ components/(chat)/chat-room-top-bar.tsx
@@ -32,10 +32,16 @@ export default function ChatRoomTopBar({
   const confirmOnClick = async () => {
     await leaveChatRoom(chatRoomId);
   };
+  const cancelAndReload = () => {
+    router.back();
+    setTimeout(() => {
+      location.reload();
+    }, 100);
+  };
   return (
     <div className="fixed top-0 w-full max-w-screen-sm mx-auto flex items-center justify-between px-5 py-3 z-20 border-neutral-200 border-b bg-white">
       <ChevronLeftIcon
-        onClick={() => router.back()}
+        onClick={cancelAndReload}
         className="size-6 cursor-pointer"
       />
       <h1 className="font-semibold text-lg text-black">{title}</h1>
